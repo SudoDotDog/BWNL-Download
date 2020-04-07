ce19640eff7985a988e18cb5f88354bf9769147f
WMXPY <wm@mengw.io>
2020-04-07 16:12:52 -0500

    replace storybook

---
+++ stories/download.story.tsx
@@ -0,0 +1,17 @@
+/**
+ * @author WMXPY
+ * @namespace Download
+ * @description Download
+ * @override Story
+ */
+
+import * as React from 'react';
+
+export default {
+    title: 'Download',
+};
+
+export const Simple = () => {
+
+    return (<div></div>);
+};
